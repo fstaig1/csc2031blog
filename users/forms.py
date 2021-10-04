fc98cb712b0bc4c214249bfc182d30c4ac5be6b1
finlay <F.Staig1@newcastle.ac.uk>
2021-10-04 14:50:33 +0100

    deleted "python again" (virtual environment)

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,8 @@ import re
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
 
-def character_check(form,field):
+
+def character_check(form, field):
     excluded_chars = "*?"
     for char in field.data:
         if char in excluded_chars:
@@ -13,8 +14,8 @@ def character_check(form,field):
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[Required(), Email()])
-    password = PasswordField(validators=[Required(), Length(min=8,max=15,message="Password must be between 8 and 15 characters in length."),character_check])
-    confirm_password = PasswordField(validators=[Required(), EqualTo("password",message="Both password fields must be equal.")])
+    password = PasswordField(validators=[Required(), Length(min=8, max=15, message="Password must be between 8 and 15 characters in length."), character_check])
+    confirm_password = PasswordField(validators=[Required(), EqualTo("password", message="Both password fields must be equal.")])
     submit = SubmitField()
 
     def validate_password(self, password):
