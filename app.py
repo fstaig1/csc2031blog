f004e2aa69f78f21cc0cf9152a957ccc26bb06ea
finlay <F.Staig1@newcastle.ac.uk>
2021-10-04 15:05:54 +0100

    added error messages 403,404,500

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
+# IMPORTS
 import socket
 from flask import Flask, render_template
 
+# CONFIG
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
 
-
+# HOME PAGE VIEW
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# ERROR PAGE VIEWS
+@app.errorhandler(403)
+def page_forbidden(error):
+     return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def page_not_found(error):
+     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+     return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
