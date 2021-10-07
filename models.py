8e39395365b4495891c43854397ebdf234033ba6
finlay <F.Staig1@newcastle.ac.uk>
2021-10-07 13:06:03 +0100

    added system to store user accounts after registration
    updated database to hold new posts table
    
    created new templates for creating and updating posts

--- models.py
+++ models.py
@@ -1,12 +1,5 @@
 from app import db
-
-
-def init_db():
-    db.drop_all()
-    db.create_all()
-    new_user = User(username='user1@test.com', password='mysecretpassword')
-    db.session.add(new_user)
-    db.session.commit()
+from datetime import datetime
 
 
 class User(db.Model):
@@ -16,6 +9,32 @@ class User(db.Model):
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
 
+    blogs = db.relationship('Post')
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, db.ForeignKey(User.username), nullable=True)
+    created = db.Column(db.DateTime, nullable=False)
+    title = db.Column(db.Text, nullable=False, default=False)
+    body = db.Column(db.Text, nullable=False, default=False)
+
+    def __init__(self, username, title, body):
+        self.username = username
+        self.created = datetime.now()
+        self.title = title
+        self.body = body
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+    new_user = User(username='user1@test.com', password='mysecretpassword')
+    db.session.add(new_user)
+    db.session.commit()
