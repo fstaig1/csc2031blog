d451ddece84ad4d5369114c41dfb28a234ae6017
finlay <F.Staig1@newcastle.ac.uk>
2021-10-18 14:43:58 +0100

    added 3 login limit

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, url_for, redirect
+from flask import Blueprint, render_template, request, flash, url_for, redirect, session
 from flask_login import login_user, logout_user
 from app import db
 from blog.forms import LoginForm
@@ -35,18 +35,39 @@ def register():
 
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
+
+    # if session attribute logins does not exist create attribute logins
+    if not session.get('logins'):
+        session['logins'] = 0
+    # if login attempts is 3 or more create an error message
+    elif session.get('logins') >= 3:
+        flash('Number of incorrect logins exceeded')
+
     form = LoginForm()
 
     if form.validate_on_submit():
 
+        # increase login attempts by 1
+        session['logins'] += 1
+
         user = User.query.filter_by(username=form.username.data).first()
 
         if not user or not check_password_hash(user.password, form.password.data):
-            flash('Please check your login details and try again')
+
+            # if no match create appropriate error message based on login attempts
+            if session['logins'] == 3:
+                flash('Number of incorrect logins exceeded')
+            elif session['logins'] == 2:
+                flash('Please check your login details and try again. 1 login attempt remaining')
+            else:
+                flash('Please check your login details and try again. 2 login attempts remaining')
 
             return render_template('login.html', form=form)
 
         if pyotp.TOTP(user.pinkey).verify(form.pinkey.data):
+
+            # if user is verified reset login attempts to 0
+            session['logins'] = 0
 
             login_user(user)
 
